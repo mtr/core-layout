4a3c2b69463874f84a519ed3307817cded175605
Martin Thorsen Ranang <mtr@ranang.org>
2015-06-21 13:10:24 +0200

    Update 2015-06-21T11:10:24.124Z

--- examples/js/bundle.js
+++ examples/js/bundle.js
@@ -64,7 +64,7 @@ module.exports = angular.module('myApp');
 
 },{"../../dist/lib/core-layout.js":2,"./components/drawer/drawer.js":12,"./components/header/header.js":14,"./components/version/version.js":16,"./demos/demos.js":17,"./home/home.js":21,"angular-messages":5,"angular-ui-router":6,"angular-x":7,"bootstrap":9,"lodash":11}],2:[function(require,module,exports){
 /**
- * @license core-layout v4.3.0, 2015-06-21T13:07:51+0200
+ * @license core-layout v4.3.1, 2015-06-21T13:09:47+0200
  * (c) 2015 Martin Thorsen Ranang <mtr@ranang.org>
  * License: MIT
  */
@@ -57550,8 +57550,8 @@ module.exports = angular
     .module('myApp.version', [
         require('./version.directive.js').name
     ])
-    .value('version', '4.3.0')
-    .value('buildTimestamp', '2015-06-21T13:07:54+0200');
+    .value('version', '4.3.1')
+    .value('buildTimestamp', '2015-06-21T13:09:51+0200');
 
 },{"./version.directive.js":15,"angular-x":7}],17:[function(require,module,exports){
 'use strict';
