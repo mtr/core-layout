a8085e445535e4af5df84d5522e46ae3aa77db8a
Martin Thorsen Ranang <mtr@ranang.org>
2016-02-15 15:20:54 +0100

    Update 2016-02-15T14:20:52.307Z

--- examples/js/bundle.js
+++ examples/js/bundle.js
@@ -1,6 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /**
- * @license core-layout v5.5.0, 2016-01-27T14:44:56+0100
+ * @license core-layout v5.5.1, 2016-02-15T15:20:09+0100
  * (c) 2016 Martin Thorsen Ranang <mtr@ranang.org>
  * License: MIT
  */
@@ -268,6 +268,28 @@
             }
         };
     }
+
+    function _fixViewportHeight() {
+        /**
+         * To cope with Facebook's embedded browser on iOS 9.x breaking the
+         * layout because the page's height gets wrong.
+         **/
+        var html = document.querySelector('html');
+
+        function _onResize(event) {
+            html.style.height = window.innerHeight + 'px';
+        }
+
+        window.addEventListener('resize', _.debounce(_onResize, 125, {
+            leading: true,
+            maxWait: 250,
+            trailing: true
+        }));
+
+        _onResize();
+    }
+
+    _fixViewportHeight();
 
     return angular
         .module('coreLayout', [angularIscroll.name, 'coreLayout.templates'])
@@ -63335,8 +63357,8 @@ module.exports = angular
     .module('myApp.version', [
         require('./version.directive.js').name
     ])
-    .value('version', '5.5.0')
-    .value('buildTimestamp', '2016-01-27T14:44:59+0100');
+    .value('version', '5.5.1')
+    .value('buildTimestamp', '2016-02-15T15:20:14+0100');
 
 },{"./version.directive.js":16,"angular-x":7}],18:[function(require,module,exports){
 'use strict';
